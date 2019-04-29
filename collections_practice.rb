1f5b3592f6dff647c6bd2ea379cb3fb9b936d431
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 20:13:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -156,7 +156,7 @@ def organize_schools(schools)
   # binding.pry
   schools.values.uniq.sort_by{|k,v| v}.map do |item|
     {item[:location] => schools.select{|school,loc| loc[:location] == item[:location]}.keys}
-  end.to_h
+  end
 end
 
 puts
