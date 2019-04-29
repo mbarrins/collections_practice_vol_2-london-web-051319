4d5194c8d4d0f0e84cff4a08b2c8086325270e37
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 20:20:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,87 @@
+require "pry"
+
+keys =  [
+        {
+            :first_name => "blake",
+                 :motto => "Have a koala-ty day!"
+        },
+        {
+            :first_name => "ashley",
+                 :motto => "I dub thee, 'Lady Brett Ashley'."
+        }
+    ]
+data = [
+           {
+             "blake" => {
+                :awesomeness => 10,
+                     :height => "74",
+                  :last_name => "johnson"
+            },
+            "ashley" => {
+                :awesomeness => 9,
+                     :height => 60,
+                  :last_name => "dubs"
+            }
+        }
+    ]
+
+merged_data = [
+        {
+             :first_name => "blake",
+            :awesomeness => 10,
+                 :height => "74",
+              :last_name => "johnson",
+                  :motto => "Have a koala-ty day!"
+
+        },
+        {
+             :first_name => "ashley",
+            :awesomeness => 9,
+                 :height => 60,
+              :last_name => "dubs",
+                  :motto => "I dub thee, 'Lady Brett Ashley'."
+
+        }
+    ]
+
+cool = [
+            {
+                   :name => "ashley",
+            :temperature => "sort of cool"
+        },
+            {
+                   :name => "blake",
+            :temperature => "cool"
+        }
+    ]
+
+schools = {
+      "flatiron school bk" => {
+        :location => "NYC"
+      },
+      "flatiron school" => {
+        :location => "NYC"
+      },
+      "dev boot camp" => {
+        :location => "SF"
+      },
+      "dev boot camp chicago" => {
+        :location => "Chicago"
+      },
+      "general assembly" => {
+        :location => "NYC"
+      },
+      "Hack Reactor" => {
+        :location => "SF"
+      }
+    }
+
+organized_schools =
+    {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+     "SF"=>["dev boot camp", "Hack Reactor"],
+     "Chicago"=>["dev boot camp chicago"]}
+
+
 def begins_with_r(arr)
   arr.all?{|item| item[0].downcase == "r"}
 end
@@ -9,6 +93,10 @@ end
 def first_wa(arr)
   arr.each{|item| return item.to_s if item.to_s.downcase[0..1]=="wa"}
 end
+
+puts
+puts first_wa(["candy", :pepper, "wall", :ball, "wacky"])
+puts "Should be wall."
 
 def remove_non_strings(arr)
   arr.select{|item| item.class == String}
@@ -48,7 +136,8 @@ def find_cool(arr)
 end
 
 def organize_schools(schools)
-  schools.values.uniq.map do |item|
+  # binding.pry
+  schools.values.uniq.sort_by{|k,v| v}.map do |item|
     [item[:location], schools.select{|school,loc| loc[:location] == item[:location]}.keys]
   end.to_h
 end
